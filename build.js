c3b125a7bc13bf7eea8860e1d935dcbc04ae5fc0
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-02-15 14:43:08 +0000

    Add files via upload

--- build.js
+++ build.js
@@ -13,7 +13,7 @@ var current_routes = 0;
 
 var date = new Date();
 var metadata = {
-	app_version: '2023-05-04',
+	app_version: '2024-02-15',
 	routes_hash: '',
 	stops_hash: '',
 	retrieval_date: `${date.getUTCFullYear()}-${(date.getUTCMonth()+1).toString().padStart(2, '0')}-${date.getUTCDate().toString().padStart(2, '0')}`
