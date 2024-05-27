5d266d5193f1c7ad596d2abdab2fa1a21692c5ea
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-05-28 00:53:13 +0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -46,7 +46,7 @@ self.addEventListener('fetch', function (event) {
 self.addEventListener('install', function(e) {
 	console.log('[ServiceWorker] Install');
 	e.waitUntil(
-		caches.open(cacheName).then(function(cache) {
+		caches.open(cache_name).then(function(cache) {
 			console.log('[ServiceWorker] Caching app shell');
 			return cache.addAll(files_to_cache);
 		})
@@ -64,7 +64,7 @@ self.addEventListener('activate', function(e) {
 	e.waitUntil(
 		caches.keys().then(function(keyList) {
 			return Promise.all(keyList.map(function(key) {
-				if (key !== cacheName) {
+				if (key !== cache_name) {
 					console.log('[ServiceWorker] Removing old cache', key);
 					return caches.delete(key);
 				}
