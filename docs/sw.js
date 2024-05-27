45c6c8862fb2f160a66d50ab5893b651a8870831
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-05-28 00:38:54 +0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -48,7 +48,7 @@ self.addEventListener('install', function(e) {
 	e.waitUntil(
 		caches.open(cacheName).then(function(cache) {
 			console.log('[ServiceWorker] Caching app shell');
-			return cache.addAll(filesToCache);
+			return cache.addAll(files_to_cache);
 		})
 	);
 });
