f734cebadead4b6b76c40199a58cd9f152010635
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-09-04 23:14:26 +0300

    Convert to module

--- build_sitemap.js
+++ build_sitemap.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+import fs from "fs";
 const site = 'https://dimitar5555.github.io/sofiatraffic-schedules/';
 const url_prefix = '#';
 
@@ -7,10 +7,11 @@ function read_file(file) {
 }
 
 function init(){
-	routes = read_file('routes');
-	trips = read_file('trips');
-	directions = read_file('directions');
-	stops = read_file('stops');
+	let routes = read_file('routes');
+	let trips = read_file('trips');
+	let directions = read_file('directions');
+	let stops = read_file('stops');
+	run(routes, trips, directions, stops);
 }
 
 function generate_url_entry(hash, priority, sitemap) {
@@ -21,7 +22,7 @@ function generate_url_entry(hash, priority, sitemap) {
 	sitemap.push('</url>');
 }
 
-function run(){
+function run(routes, trips, directions, stops){
 	var sitemap = ['<?xml version="1.0" encoding="UTF-8"?>', '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'];
 	for(const page of ['schedules', 'stops_map']) {
 		generate_url_entry(`${page}`, 1, sitemap);
@@ -38,4 +39,3 @@ function run(){
 	fs.writeFileSync('docs/sitemap.xml', sitemap.join('\n'));
 }
 init();
-run();
