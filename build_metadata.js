5ba9af3f836eca5dcf512f74e05d516549a6118d
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2025-03-19 08:07:55 +0200

    Bump app version and run build_metadata

--- build_metadata.js
+++ build_metadata.js
@@ -3,7 +3,7 @@ import crypto from "crypto";
 
 function generate_metadata() {
     let metadata = {
-        app_version: '2025-01-31',
+        app_version: '2025-03-19',
         retrieval_date: new Date().toISOString().split('T')[0],
         hashes: {}
     };
@@ -15,3 +15,5 @@ function generate_metadata() {
 
     fs.writeFileSync('docs/data/metadata.json', JSON.stringify(metadata));
 }
+
+generate_metadata();
